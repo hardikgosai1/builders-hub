2064d2f7b82cbda346b7399d95807db9a4968afc
Containerman17 <8990432+containerman17@users.noreply.github.com>
2025-03-31 05:24:02 +0000

    step list

--- toolbox/src/l1-launcher/L1Launcher.tsx
+++ toolbox/src/l1-launcher/L1Launcher.tsx
@@ -1,9 +1,18 @@
 "use client";
 
+import { useState } from "react";
+import Steps from "./components/Steps";
 import ToolHeader from "./components/ToolHeader";
+import { stepGroups, stepList } from "./stepList";
 
 
 export default function L1Launcher() {
+    const [currentStep, setCurrentStep] = useState<keyof typeof stepList>("genesis");
+
+    const onReset = () => {
+        alert("TODO: Implement reset");
+    }
+
     return <>
         <div className="container mx-auto max-w-6xl p-8 ">
             <ToolHeader
@@ -14,7 +23,28 @@ export default function L1Launcher() {
                 issuePath="/tools/l1-launcher"
                 issueTitle="Update L1 Launcher Tool Information"
             />
-            <h1>L1 Launcher</h1>
+            <div className="flex flex-col lg:flex-row">
+                <div className="w-full lg:w-80 mb-8">
+                    <Steps stepGroups={stepGroups} stepList={stepList} currentStep={currentStep} maxAdvancedStep={"add-to-wallet"} advanceTo={(step) => { setCurrentStep(step) }} onReset={() => { setCurrentStep("genesis") }} />
+                    {/* Reset button */}
+                    <div className="mt-8 -ml-4 w-full">
+                        <button
+                            onClick={onReset}
+                            className="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-red-700 dark:text-red-400 bg-red-50 dark:bg-red-950/50 rounded-lg hover:bg-red-100 dark:hover:bg-red-950 border border-red-100 dark:border-red-900/50"
+                        >
+                            <svg className="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                            </svg>
+                            Start Over
+                        </button>
+                    </div>
+                </div>
+                <div className="flex-1 min-w-0">
+                    <div className="h-full">
+                        {stepList[currentStep].component}
+                    </div>
+                </div>
+            </div>
         </div>
     </>
 }
---
+++ toolbox/src/l1-launcher/stepList.tsx
@@ -0,0 +1,108 @@
+import { BookOpen, Terminal, Flag, Settings, Server } from 'lucide-react'
+import { StepGroupListType, StepListType } from './components/Steps'
+
+export const stepGroups: StepGroupListType = {
+    "welcome": {
+        title: "Welcome",
+        icon: BookOpen
+    },
+    "configure": {
+        title: "Configure",
+        icon: Settings
+    },
+    "launch-l1": {
+        title: "Launch your L1",
+        icon: Server
+    },
+    "initialize": {
+        title: "Initialize",
+        icon: Terminal
+    },
+    "whats-next": {
+        title: "What's next?",
+        icon: Flag
+    },
+}
+
+export const stepList: StepListType = {
+    "welcome": {
+        title: "Welcome",
+        component: <div>Welcome</div>,
+        group: "welcome",
+    },
+    "chain-parameters": {
+        title: "Chain Parameters",
+        component: <div>Chain Parameters</div>,
+        group: "configure",
+    },
+    "tokenomics": {
+        title: "Tokenomics",
+        component: <div>Tokenomics</div>,
+        group: "configure",
+    },
+    "permissions": {
+        title: "Permissions",
+        component: <div>Permissions</div>,
+        group: "configure",
+    },
+    "genesis": {
+        title: "Create genesis",
+        component: <div>Genesis</div>,
+        group: "configure",
+    },
+    "prepare-validators": {
+        title: "Prepare Validators",
+        component: <div>Prepare Validators</div>,
+        group: "launch-l1",
+    },
+    "fund-p-chain-wallet": {
+        title: "Fund P-chain wallet",
+        component: <div>Fund P-chain wallet</div>,
+        group: "launch-l1",
+    },
+    "create-chain": {
+        title: "Create chain",
+        component: <div>Create chain</div>,
+        group: "launch-l1",
+    },
+    "launch-validators": {
+        title: "Launch validators",
+        component: <div>Launch validators</div>,
+        group: "launch-l1",
+    },
+    "convert-to-l1": {
+        title: "Convert to L1",
+        component: <div>Convert to L1</div>,
+        group: "launch-l1",
+    },
+    "launch-rpc-node": {
+        title: "Launch an RPC node",
+        component: <div>Launch an RPC node</div>,
+        group: "launch-l1",
+    },
+    "open-rpc-port": {
+        title: "Open RPC port",
+        component: <div>Open RPC port</div>,
+        group: "launch-l1",
+    },
+    "add-to-wallet": {
+        title: "Add to wallet",
+        component: <div>Add to wallet</div>,
+        group: "initialize",
+    },
+    "deploy-contracts": {
+        title: "Deploy contracts",
+        component: <div>Deploy contracts</div>,
+        group: "initialize",
+    },
+    "initialize-validator-manager": {
+        title: "Initialize validator manager",
+        component: <div>Initialize validator manager</div>,
+        group: "initialize",
+    },
+    "whats-next": {
+        title: "What's next?",
+        component: <div>What's next?</div>,
+        group: "whats-next",
+    }
+}
