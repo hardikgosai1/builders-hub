791687a613328f3ef5db0b2124d426cc5c8b9a9e
federiconardelli7 <federico.nardelli7@gmail.com>
2025-04-16 00:47:08 +0200

    add metadata academy og

---
+++ app/(home)/academy/opengraph-image.tsx
@@ -0,0 +1,135 @@
+import { ImageResponse } from 'next/og';
+
+export const runtime = 'edge';
+
+// Regular fonts
+const medium = fetch(new URL('../../api/og/Geist-Medium.ttf', import.meta.url)).then((res) =>
+  res.arrayBuffer(),
+);
+
+const light = fetch(new URL('../../api/og/Geist-Light.ttf', import.meta.url)).then((res) =>
+  res.arrayBuffer(),
+);
+
+const regular = fetch(new URL('../../api/og/GeistMono-Light.ttf', import.meta.url)).then((res) =>
+  res.arrayBuffer(),
+);
+
+// Image metadata
+export const alt = 'Avalanche Academy';
+export const size = {
+  width: 1200, 
+  height: 630,
+};
+
+export const contentType = 'image/png';
+
+export default async function Image() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: 'flex',
+          height: '100%',
+          width: '100%',
+          position: 'relative',
+          backgroundColor: '#fafafa',
+          overflow: 'hidden',
+          alignItems: 'center',
+          backgroundImage: "url('https://build.avax.network/og.png')",
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat'
+        }}
+      >
+        <div
+          style={{
+            maxWidth: '56rem',
+            display: 'flex',
+            flexDirection: 'column',
+            padding: '0 70px',
+            width: '100%',
+          }}
+        >
+          <h1
+            style={{
+              fontSize: '5.35rem',
+              fontFamily: "Geist-Medium",
+              lineHeight: '1.3',
+              letterSpacing: '-0.015em',
+              margin: '0 0 30px 0',
+              color: 'black',
+            }}
+          >
+            Avalanche Academy
+          </h1>
+          
+          <p
+            style={{
+              fontSize: '2.35rem',
+              color: '#4b5563',
+              maxWidth: '110%',
+              margin: '0 0 30px 0',
+              lineHeight: 1.4,
+              letterSpacing: '-0.01em',
+              fontFamily: "Geist-Light" 
+            }}
+          >
+            The Learning Platform for Avalanche Ecosystem.
+          </p>
+          
+          <div
+            style={{
+              marginTop: '4.5rem',
+              display: 'flex',
+              alignItems: 'center'
+            }}
+          >
+            <div
+              style={{
+                borderRadius: '0.375rem',
+                fontSize: '2rem',
+                border: '1.25px solid #71dbff',
+                backgroundColor: 'white',
+                fontFamily: "Geist-Mono",
+                color: 'black',
+                padding: '12px',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="329.2092783505155 0 271.6216494845361 229.44"
+                width="26"
+                height="24"
+              >
+                <g>
+                  <path
+                    fill="red"
+                    d="M532.763 137.242C536.794 130.375 546.806 130.375 550.793 137.242L593.631 210.426C597.662 217.292 592.612 225.842 584.594 225.842H498.917C490.899 225.842 485.893 217.292 489.88 210.426L532.718 137.242H532.763Z"
+                  />
+                  <path
+                    fill="red"
+                    d="M506.887 88.5117C510.785 81.6895 510.785 73.2724 506.887 66.4059L471.757 5.09425C467.814 -1.7723 457.979 -1.7723 454.037 5.09425L336.464 210.338C332.521 217.204 337.438 225.798 345.324 225.798H415.54C423.381 225.798 430.602 221.59 434.5 214.768L506.843 88.4674L506.887 88.5117Z"
+                  />
+                </g>
+              </svg>
+              <span style={{paddingLeft: "10px"}}>build.avax.network/<span style={{color: "red"}}>academy</span></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    ),
+    {
+      ...size,
+      fonts: [
+        { name: 'Geist-Medium', data: await medium, weight: 600 },
+        { name: 'Geist-Mono', data: await regular, weight: 500 },
+        { name: 'Geist-Light', data: await light, weight: 300 }
+      ],
+    }
+  );
+} 
