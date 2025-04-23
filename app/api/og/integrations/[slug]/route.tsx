2c30c143993f62e97fd63856577629efc24f3d55
federiconardelli7 <federico.nardelli7@gmail.com>
2025-04-23 12:49:18 +0200

    add explicit promise with return type in routes

--- app/api/og/integrations/[slug]/route.tsx
+++ app/api/og/integrations/[slug]/route.tsx
@@ -1,9 +1,12 @@
 import type { NextRequest } from 'next/server';
+import { ImageResponse } from 'next/og';
 import { loadFonts, createOGResponse } from '@/utils/og-image';
 
 export const runtime = 'edge';
 
-export async function GET(request: NextRequest) {
+export async function GET(
+  request: NextRequest,
+): Promise<ImageResponse> {
   const { searchParams } = request.nextUrl;
   const title = searchParams.get('title'),
     description = searchParams.get('description');
