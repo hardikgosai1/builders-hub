257e352e192074dcb3f0a370b8a33837c01297f0
navillanueva <nicolasarnedo99@gmail.com>
2025-05-16 11:05:15 +0200

    renamed folder and included more specific links for redirects

--- next.config.mjs
+++ next.config.mjs
@@ -41,37 +41,37 @@ const config = {
       // Redirects from old VM paths
       {
         source: '/docs/virtual-machines/default-precompiles/index',
-        destination: '/docs/avalanche-l1s/subnetEVM-configuration/evm-l1-customization',
+        destination: '/docs/avalanche-l1s/evm-configuration/evm-l1-customization#precompiles',
         permanent: true,
       },
       {
         source: '/docs/virtual-machines/default-precompiles/deployerallowlist',
-        destination: '/docs/avalanche-l1s/subnetEVM-configuration/permissions',
+        destination: '/docs/avalanche-l1s/evm-configuration/permissions#contract-deployer-allowlist',
         permanent: true,
       },
       {
         source: '/docs/virtual-machines/default-precompiles/txallowlist',
-        destination: '/docs/avalanche-l1s/subnetEVM-configuration/permissions',
+        destination: '/docs/avalanche-l1s/evm-configuration/permissions#transaction-allowlist',
         permanent: true,
       },
       {
         source: '/docs/virtual-machines/default-precompiles/contractnativeminter',
-        destination: '/docs/avalanche-l1s/subnetEVM-configuration/tokenomics',
+        destination: '/docs/avalanche-l1s/evm-configuration/tokenomics#native-minter',
         permanent: true,
       },
       {
         source: '/docs/virtual-machines/default-precompiles/feemanager',
-        destination: '/docs/avalanche-l1s/subnetEVM-configuration/transaction-fees',
+        destination: '/docs/avalanche-l1s/evm-configuration/transaction-fees#fee-manager',
         permanent: true,
       },
       {
         source: '/docs/virtual-machines/default-precompiles/rewardmanager',
-        destination: '/docs/avalanche-l1s/subnetEVM-configuration/transaction-fees',
+        destination: '/docs/avalanche-l1s/evm-configuration/transaction-fees#reward-manager',
         permanent: true,
       },
       {
         source: '/docs/virtual-machines/default-precompiles/warpmessenger',
-        destination: '/docs/avalanche-l1s/subnetEVM-configuration/warpmessenger',
+        destination: '/docs/avalanche-l1s/evm-configuration/warpmessenger',
         permanent: true,
       }
     ];
