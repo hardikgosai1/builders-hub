79b4e6ee595116498bb63ba1f12910484c247f4e
Containerman17 <8990432+containerman17@users.noreply.github.com>
2025-09-11 06:17:00 +0000

    remove unused components: deleted SplashPage, ToolboxApp, and CollectConversionSignatures files to streamline the codebase

--- types/global.d.ts
+++ types/global.d.ts
@@ -3,5 +3,34 @@ interface Window {
         request: (args: { method: string; params?: any[] }) => Promise<any>;
         on: (event: string, callback: (...args: any[]) => void) => void;
         removeListener: (event: string, callback: (...args: any[]) => void) => void;
+        selectedAddress?: string;
+        chainId?: string;
+        isMetaMask?: boolean;
+        isConnected?: () => boolean;
     };
-} 
+}
+
+declare global {
+    interface Window {
+        ethereum?: {
+            request: (args: { method: string; params?: any[] }) => Promise<any>;
+            on: (event: string, callback: (...args: any[]) => void) => void;
+            removeListener: (event: string, callback: (...args: any[]) => void) => void;
+            selectedAddress?: string;
+            chainId?: string;
+            isMetaMask?: boolean;
+            isConnected?: () => boolean;
+        };
+        avalanche?: {
+            request: <T>(args: {
+                method: string;
+                params?: Record<string, unknown> | unknown[];
+                id?: number;
+            }) => Promise<T>;
+            on<T>(event: string, callback: (data: T) => void): void;
+            removeListener(event: string, callback: () => void): void;
+        };
+    }
+}
+
+export { }; 
