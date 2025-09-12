082a75f6a8953829de59b761c01e1f1d13b0646a
Containerman17 <8990432+containerman17@users.noreply.github.com>
2025-09-12 06:18:05 +0000

    chore: update STORE_VERSION to v4

--- components/toolbox/stores/utils.ts
+++ components/toolbox/stores/utils.ts
@@ -1,2 +1,2 @@
 export const localStorageComp = () => typeof window !== 'undefined' ? localStorage : { getItem: () => null, setItem: () => { }, removeItem: () => { } }
-export const STORE_VERSION = "v1"
+export const STORE_VERSION = "v4"
